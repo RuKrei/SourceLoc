50601895f4fe91c60f08699d3fd2df28185a867a
Rudi Kreidenhuber <Rudi.Kreidenhuber@gmail.com>
2021-10-27 16:31:04 +0200

    conn toolboxes brainstorming

---
+++ src/connectivist.py
@@ -0,0 +1,6 @@
+# Tools to use:
+# networkX - https://networkx.org/documentation/stable/index.html
+# frites - https://brainets.github.io/frites/index.html
+# mne connectivity
+# bctpy - https://github.com/aestrivex/bctpy
+
